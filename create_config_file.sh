06a18c6c2bd6bccfac56e68fd3b3e342df2751cc
ctibordudahcl <86057782+ctibordudahcl@users.noreply.github.com>
2025-01-30 21:31:50 +0100

    Update create_config_file.sh

--- create_config_file.sh
+++ create_config_file.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 scanLatestCommitFiles='yes'
 
 if [[ -z "$scanLatestCommitFiles" || ( "$scanLatestCommitFiles" != "yes" && "$scanLatestCommitFiles" != "no" ) ]]; then
@@ -9,8 +9,8 @@ fi
 if [ "$scanLatestCommitFiles" = 'yes' ]; then
   echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><Configuration enableSecrets=\"true\" sourceCodeOnly=\"true\" staticAnalysisOnly=\"true\"><Targets><Target path=\"$PWD\"></Target></Targets></Configuration>" > appscan-config.xml
   echo "Only these files will be scanned:"
-  echo $(git diff --name-only HEAD HEAD∼1)
-  diffFilesList=$(git diff --name-only HEAD HEAD∼1)
+  echo $(git diff --name-only HEAD HEAD~1)
+  diffFilesList=$(git diff --name-only HEAD HEAD~1)
   readarray -t diffFiles <<< "$diffFilesList"
   for i in ${diffFiles[@]}; do sed -i "s|\(</Target>\)|<Include>$i</Include>\1|" appscan-config.xml; done
 else
